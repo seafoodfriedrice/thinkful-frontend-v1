e4785cc74f001fc561f1df6841a2968e377c3446
Rodolf Sabalburo <rodolf.sabalburo@gmail.com>
2013-09-24 22:36:08 -1000

    HotCold: refactor application.js according to mentor Morgan's suggestions
    Eliminate use of global variables.
    
    Save selectors as variables, e.g. var $response = $('#response');
        Using $ in front of variable is best practice to signify jQuery object.
    
    Make event function clickGuess() reusable.
    
    Place message responses into variables.
    
    Overall code cleanup in several areas to make code elegant and easier to read.

--- HotCold/application.js
+++ HotCold/application.js
@@ -4,52 +4,69 @@ $(document).ready(function() {
         console.log(r);
         return r;
     }
-    win = random();
-    tries = 0;
+    var win = random();
+    var tries = 0;
 
-    var hotOrCold = function(guess, win) {
+    var $response = $('#response');
+    var $field = $('#guess-field');
+    var $guess = $('#guess');
+
+    function hotOrCold(guess, win) {
         tries++;
+
         var difference = Math.abs(guess - win);
         var hint = (guess > win) ? "too high.<br />" : "too low.<br />";
-        if (guess > 100) {
-            reply = "Number is greater than 100. Try again.<br />";
-        } else if (guess == win) {
-            reply = "We have a winner! It took you " + tries + " tries!";
-            $('#response').html('');
-            $('#guess-field, #guess').prop('disabled', true);
-        } else if (guess == "") {
-            reply = "I can't read your mind. You need to enter a number.<br />";
-        } else if (guess == 0) {
-            reply = "There's always someone like you that will guess 0" +
-                " when you give a range of from 1-100.<br />";
-        } else if (isNaN(guess)) {
-            reply = "That isn't a number.<br />";
-        } else if (difference < 20 && difference >= 5) {
-            reply = "Getting warmer. But still " + hint;
-        } else if (difference < 5) {
-            reply = "Burning up! Just slightly " + hint;
-        } else {
-            reply = "You are colder than a polar bear's" +
-                " toenail because you are way " + hint;
+	    var reply;
+
+        var MSG_SPECIFY_NUMBER = "Please specify a number between 1-100.<br />";
+        var MSG_WINNER = guess + "? We have a winner! It took you " + tries + " tries!"
+        var MSG_WARMER = guess + "? Getting warmer. But still " + hint;
+        var MSG_BURNING = guess + "? Burning up! Just slightly " + hint;
+        var MSG_COLD = guess + "? You are colder than a polar bear's" +
+                       " toenail because you are way " + hint;
+
+
+        if (guess > 100 || guess <= 0 || guess == "" || isNaN(guess)) {
+            $response.append(MSG_SPECIFY_NUMBER);
+            tries--; // User mistake doesn't count as a try
+            return;
         }
-        $('#response').append(reply);
+
+        if (guess == win) {
+            $response.html(MSG_WINNER);
+            // jquery .add() allows passing of
+            // two objects in one method chain
+            $($field).add($guess).prop('disabled', true);
+            return;
+        }
+        
+        if (difference < 20 && difference >= 5) {
+            $response.append(MSG_WARMER);
+            return;
+        } else if (difference < 5) {
+            $response.append(MSG_BURNING);
+            return;
+        } else {
+            $response.append(MSG_COLD);
+            return;
+        }
     };
 
-    $('#guess-field').keydown(function(e) {
-        if(e.keyCode == 13) {
-            $('#guess').click();
-        }
-    });
-    $('#guess').click(function() {
-        g = $('#guess-field');
-        hotOrCold(g.val(), win);
-        g.val(''); 
+    $field.keydown(function(e) {
+        if (e.keyCode == 13)
+            clickGuess(e);
     });
 
+    $guess.click(clickGuess);
+    
+    function clickGuess(e) {
+        hotOrCold($field.val(), win);
+        $field.val(''); 
+    }
+
     $('#new-game').click(function() {
-        win = random();
-        $('#response').html('');
-        $('#guess-field, #guess').prop('disabled', false);
-        tries = 0;
+        win = random(), tries = 0;
+        $response.html('');
+        $($field).add($guess).prop('disabled', false);
     });
 });

31e37c4d76c4d2af337efd0ca674c837077065e4
Rodolf Sabalburo <rodolf.sabalburo@gmail.com>
2013-10-28 07:03:13 -1000

    ShoppingList: mentor recommended changes for application.js

--- ShoppingList/application.js
+++ ShoppingList/application.js
@@ -1,20 +1,35 @@
 $(document).ready(function() {
     var $itemField = $('#item-field');
     var $addItem = $('#add-btn');
+    var $items = $('#items');
+
+    function itemLength() {
+        return $itemField.val().length >= 2;
+    }
+
     $itemField.keydown(function(e) {
-        if (e.keyCode == 13) {
+        if (e.keyCode == 13 && itemLength(e)) {
             clickAddItem(e);
         }
     });
-    $addItem.click(clickAddItem);
+
+    $addItem.click(function(e) {
+        itemLength(e) && clickAddItem(e);
+    });
+
     function clickAddItem(e) {
-        var $items = $('#items');
-        var item = '<li><span class="item">' + $itemField.val() + '</span></li>';
+        var item = '<li><span class="item">' + $itemField.val() + '</span>' +
+            '<span class="item-btn" style="display:none">x</span></li>';
         $items.append(item);
         $itemField.val('');
     }
-    $('#items').on('click', 'li', function() {
-        var item = $(this).find('.item');
+
+    $items.on('click', '.item', function() {
+        var item = $(this);
+        var remove = item.parent().find('.item-btn').toggle();
         item.toggleClass('strikethrough');
+    });
+    $items.on('click', '.item-btn', function() {
+        $(this).parent().remove();
     });
 });

1e7d3684d6576fe2c01466b9d067553e80fbc179
Rodolf Sabalburo <rodolf.sabalburo@gmail.com>
2013-11-04 07:08:10 -1000

    ShoppingList: add comment block for updated on('click') function

--- ShoppingList/application.js
+++ ShoppingList/application.js
@@ -32,4 +32,19 @@ $(document).ready(function() {
     $items.on('click', '.item-btn', function() {
         $(this).parent().remove();
     });
+    /*
+    $items.on('click', function(e) {
+        var target = e.target;
+        var item = $(target).parent("li");
+
+        if (target.className.indexOf("item") !== -1) {
+            item.toggleClass('strikethrough');
+            item.find('.item-btn').toggle();
+            console.log("item clicked.");
+        } else if (target.className.indexOf("-btn") !== -1) {
+            item.parent.remove();
+            console.log("item-btn clicked.");
+        }
+    });
+    */
 });
